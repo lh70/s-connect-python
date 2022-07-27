d614edce2af89fdd9e2d3a35ba47b7b8390cd206
Lukas Holst <lukas@familie-holst.de>
2022-07-27 14:41:01 +0200

    outsourced wlan connectivity into module

--- boot.py
+++ boot.py
@@ -6,64 +6,23 @@ We will activate WiFi station-connection, but not WebREPl (web cli) as it is not
 """
 
 import network
-import time
 import machine
 # import webrepl
 
 
-WIFI_DEVICE_NAME = 'ESP32-6'
-
-WIFI_CREDENTIALS = {
-}  # ssid: password
-
-WEBREPL_PASSWORD = 'esp32batch'
-
-
-wlan = network.WLAN(network.STA_IF)
-wlan.active(True)
-wlan.config(dhcp_hostname=WIFI_DEVICE_NAME)
-
 try:
-    while not wlan.isconnected():
-        print('scanning for wlan:')
-
-        wlan_list = wlan.scan()
-        if not wlan_list:
-            print('  empty list -> must restart wlan interface')
-            wlan.active(False)
-            wlan.active(True)
-            continue
-
-        for ssid, bssid, _, _, _, _ in wlan_list:
-            ssid = ssid.decode()
-            print(f' found: {ssid}...', end='')
-
-            if ssid in WIFI_CREDENTIALS:
-                print('credentials provided...connecting...', end='')
-                wlan.connect(ssid, WIFI_CREDENTIALS[ssid], bssid=bssid)
-
-                start_time = time.ticks_ms()
-                while (not wlan.isconnected()) and (time.ticks_ms() - start_time < 10000):
-                    time.sleep_ms(100)
-
-                if wlan.isconnected():
-                    print('SUCCESS')
-                    break
-                else:
-                    print('FAILED...trying next one')
-            else:
-                print('no credentials provided')
-
-        if not wlan.isconnected():
-            print('No connectable network found. Retrying in 10 seconds.')
-            time.sleep_ms(10000)
+    from lh_lib.network_stack.wlan import connect
+    connect()
 except KeyboardInterrupt:
     print('interrupt by user...moving on without wlan\n')
+except ImportError:
+    print('lh_lib does not exist...moving on without wlan\n')
 else:
-    print(f'network config: {wlan.ifconfig()}\n')
+    print(f'network config: {network.WLAN(network.STA_IF).ifconfig()}\n')
 
 machine.freq(240000000)
 print('adjusted machine frequency to max: 240MHz (default: 160MHz)\n\n')
 
+# WEBREPL_PASSWORD = 'esp32batch'
 # webrepl.start(password=WEBREPL_PASSWORD)
 # print("webREPL started with password: {}".format(WEBREPL_PASSWORD))
---
+++ lh_lib/network_stack/wlan.py
@@ -0,0 +1,83 @@
+import json
+import time
+import network
+
+from lh_lib.logging import log
+
+
+try:
+    with open('wlan.json') as f:
+        wlan_config = json.load(f)
+    hostname = wlan_config['HOSTNAME']
+    wlan_credentials = wlan_config['WLAN_CREDENTIALS']
+    assert len(wlan_credentials) > 0
+except OSError:
+    raise Exception('wlan config does not exist, but is mandatory')
+except ValueError:
+    raise Exception('wlan config is no valid json')
+except KeyError:
+    raise Exception('wlan config must contain a HOSTNAME and non empty WLAN_CREDENTIALS')
+except AssertionError:
+    raise Exception('WLAN_CREDENTIALS may not be empty')
+
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+time.sleep_us(100)  # bug in micropython 19.1, hostname may not be set directly after wlan activation
+wlan.config(dhcp_hostname=hostname)
+
+latest_connected_network = None
+
+
+def connect():
+    global latest_connected_network
+
+    while not wlan.isconnected():
+        wlan_list = wlan.scan()
+
+        if not wlan_list:
+            # empty list on active wlan bug -> must restart wlan interface
+            wlan.active(False)
+            wlan.active(True)
+            continue
+
+        for ssid, _, _, _, _, _ in wlan_list:
+            ssid = ssid.decode()
+
+            if ssid in wlan_credentials:
+                wlan.connect(ssid, wlan_credentials[ssid])
+
+                start_time = time.ticks_ms()
+                while (not wlan.isconnected()) and (time.ticks_diff(time.ticks_ms(), start_time) < 5000):
+                    time.sleep_ms(20)
+
+                if wlan.isconnected():
+                    log('connected to wlan: {}', ssid)
+                    latest_connected_network = ssid
+                    break
+        if not wlan.isconnected():
+            log('no connectable network found. retrying...')
+            time.sleep_ms(200)
+
+
+def reconnect():
+    if wlan.isconnected():
+        return
+
+    if latest_connected_network is None:
+        raise Exception('reconnect is called before connect')
+
+    # make sure that the wlan module does not hang
+    if not wlan.scan():
+        wlan.active(False)
+        wlan.active(True)
+
+    ssid = latest_connected_network
+    wlan.connect(ssid, wlan_credentials[ssid])
+
+    start_time = time.ticks_ms()
+    while (not wlan.isconnected()) and (time.ticks_diff(time.ticks_ms(), start_time) < 5000):
+        time.sleep_us(100)
+
+
+def isconnected():
+    return wlan.isconnected()
