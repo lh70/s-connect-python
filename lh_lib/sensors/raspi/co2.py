b7378a743491404f680f660fa7fa97f7dd119904
Lukas Holst <lukas@familie-holst.de>
2022-09-28 14:20:04 +0200

    added CO2 sensor implementation for raspberrypi

---
+++ lh_lib/sensors/raspi/co2.py
@@ -0,0 +1,81 @@
+from gpiozero import InputDevice
+from lh_lib.time import ticks_us, ticks_us_diff_to_current
+
+from lh_lib.sensors.sensor import AbstractSensor
+
+MIN_PULSE_DURATION_US = 2000
+
+
+class CO2(AbstractSensor):
+
+    """
+    Initialises a the 5000ppm CO2 sensor MH-Z19B to retrieve its values via the pwm output interrupt driven.
+
+    pin:integer should be one of: (GPIO) 5, 6, 16, 17, 22, 23, 24, 25, 26, 27
+        further information about the GPIO: https://pinout.xyz
+    """
+    def __init__(self, pin=17):
+        self.input_device = InputDevice(pin=pin)
+        super().__init__()
+
+        self.high_pulse = 0
+        self.low_pulse = 0
+        self.pulses_changed = False
+        self.irq_fresh_start = True
+
+        self.last_interrupt_time = ticks_us()
+
+    """
+    if new pulse values available sets the sensors value to the set(high_pulse, low_pulse, computed_concentration)
+    else sets value to None, which represents an invalid sensor value that will be ignored.
+    """
+    def update(self):
+        if self.pulses_changed:
+            if self.high_pulse >= MIN_PULSE_DURATION_US and self.low_pulse >= MIN_PULSE_DURATION_US:
+                self.value = self.high_pulse, self.low_pulse, self._pulses_to_concentration(self.high_pulse, self.low_pulse)
+            self.pulses_changed = False
+        else:
+            self.value = None
+
+    """
+    registers the interrupt handler used to calculate the co2 concentration
+    """
+    def start_irq(self):
+        self.irq_fresh_start = True
+        # default is to listen for both edge kinds
+        self.input_device.pin.when_changed = self._interrupt
+
+    """
+    removes the interrupt handler
+    """
+    def stop_irq(self):
+        self.input_device.pin.when_changed = None
+
+    """
+    interrupt handler which sets the pulse durations
+    """
+    def _interrupt(self, ticks, state):
+        # determine the time duration since last interrupt
+        duration = ticks_us_diff_to_current(self.last_interrupt_time)
+        # on the rising edge set the low pulse duration
+        if state:
+            self.low_pulse = duration
+            # one high + low pulse duration pair are valid together, so switch output to the new valid pair
+            # ignore second measurement if there is no valid first measurement
+            if not self.irq_fresh_start:
+                self.pulses_changed = True
+        # on the falling edge set the high pulse duration
+        else:
+            self.high_pulse = duration
+            # set true to indicate a valid first measurement
+            if self.irq_fresh_start:
+                self.irq_fresh_start = False
+        # reset the time since last interrupt
+        self.last_interrupt_time = ticks_us()
+
+    """
+    utility function for later use which converts the pulse lengths into a co2 concentration in ppm
+    """
+    def _pulses_to_concentration(self, high_pulse, low_pulse):
+        return (5000 * (high_pulse - 2000)) / (high_pulse + low_pulse - 4000)
+
--- lh_lib/time.py
+++ lh_lib/time.py
@@ -17,3 +17,19 @@ def ticks_ms_diff_to_current(old_ticks):
     # on CPython we cannot use ticks, but it is assumed fast enough to use time_ns()
     # time_ns() is converted to milliseconds with rounding using only integer arithmetic
     return (time.perf_counter_ns() + 500000) // 1000000 - old_ticks
+
+
+def ticks_us():
+    if RUNNING_MICROPYTHON:
+        return time.ticks_us()
+    # on CPython we cannot use ticks, but it is assumed fast enough to use time_ns()
+    # time_ns() is converted to microseconds with rounding using only integer arithmetic
+    return (time.perf_counter_ns() + 500) // 1000
+
+
+def ticks_us_diff_to_current(old_ticks):
+    if RUNNING_MICROPYTHON:
+        return time.ticks_diff(time.ticks_us(), old_ticks)
+    # on CPython we cannot use ticks, but it is assumed fast enough to use time_ns()
+    # time_ns() is converted to microseconds with rounding using only integer arithmetic
+    return (time.perf_counter_ns() + 500) // 1000 - old_ticks
